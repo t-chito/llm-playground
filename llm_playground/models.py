44b5c1afbd6237912441c04d5ab7665cabd8333a
t-chito <published.address.of.k.t@gmail.com>
2024-03-31 03:58:46 +0900

    KA カードのプロンプトを少し変更してみた
    結果は芳しくない

--- llm_playground/models.py
+++ llm_playground/models.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
 from openai import OpenAI
 
-llm_openai = ChatOpenAI(temperature=0, model="gpt-4-turbo-preview")
+llm_openai = ChatOpenAI(temperature=0, model="gpt-4-turbo-preview", max_tokens=4000)
 client = OpenAI
